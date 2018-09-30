0d012ba8a5a79aebb9c2c2e4e8107c026b52f8b8
C-A de Salaberry <cadesalaberry@yahoo.com>
2018-09-30 17:51:30 +0200

    Update

---
+++ docs/precache-manifest.f6e4ab46b84eec4a52ee3755b270a5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "03fe64987236481882a0",
+    "url": "/js/chunk-vendors.b06b9ea5.js"
+  },
+  {
+    "revision": "31a21953c7122fd8624f",
+    "url": "/js/app.f5e1701c.js"
+  },
+  {
+    "revision": "9ef8a4801d72c41c8dc06fe546eef82f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03fe64987236481882a0",
+    "url": "/css/chunk-vendors.7e075314.css"
+  },
+  {
+    "revision": "31a21953c7122fd8624f",
+    "url": "/css/app.da149651.css"
+  }
+];
