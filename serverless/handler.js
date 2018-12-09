5c9808f43543e9052d246a35184da7a553325881
C-A de Salaberry <cadesalaberry@yahoo.com>
2018-12-09 18:55:58 +0100

    Upd: :sparkles: Expose endpoints to record events

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,31 +1,125 @@
 'use strict'
 
-const Handler = {}
+const AWS = require('aws-sdk')
+const Promise = require('bluebird')
+let DYNAMODB_CONFIG = { region: 'eu-west-3' }
 
-Handler.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    })
+if (process.env.NODE_ENV !== 'prodution') {
+  DYNAMODB_CONFIG = {
+    endpoint: 'http://localhost:8000',
+    region: 'localhost'
+  }
+}
+const dynamoDb = new AWS.DynamoDB.DocumentClient(DYNAMODB_CONFIG)
+
+const getInteractionEventHistory = async (event, context) => {
+  const dynamoScan = Promise.promisify(dynamoDb.scan, {
+    context: dynamoDb
+  })
+  const payload = {
+    TableName: process.env.DYNAMODB_TABLE
   }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return dynamoScan(payload)
+    .then((o) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(o)
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Error processing the request',
+          error: err
+        })
+      }
+    })
 }
 
-Handler.log_event = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    })
+const getMyInteractionEventHistory = async (event, context) => {
+  const params = event.queryStringParameters
+  const dynamoQuery = Promise.promisify(dynamoDb.query, {
+    context: dynamoDb
+  })
+  const payload = {
+    TableName: process.env.DYNAMODB_TABLE,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': params.userId
+    }
   }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return dynamoQuery(payload)
+    .then((o) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(o)
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Error processing the request',
+          error: err
+        })
+      }
+    })
 }
 
-module.exports = Handler
+const recordInteractionEvent = async (event) => {
+  const dynamoPut = Promise.promisify(dynamoDb.put, {
+    context: dynamoDb
+  })
+  let body = {}
+
+  try {
+    body = JSON.parse(event.body)
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Invalid JSON body',
+        error: error
+      })
+    }
+  }
+
+  const payload = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      userId: body.userId,
+      fingerprint: body.fingerprint,
+      actionName: body.actionName,
+      cardId: body.cardId
+    }
+  }
+
+  return dynamoPut(payload)
+    .then((o) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Event logged successfully!',
+          payload: o
+        })
+      }
+    })
+    .catch((err) => {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Error processing the request',
+          error: err
+        })
+      }
+    })
+}
+
+module.exports = {
+  getMyInteractionEventHistory,
+  getInteractionEventHistory,
+  recordInteractionEvent
+}
