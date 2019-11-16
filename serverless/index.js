d02002ce767a124c9719ce6d6fbe27d098b1d1c1
C-A de Salaberry <cadesalaberry@yahoo.com>
2019-11-17 00:32:42 +0100

    feat(api): :sparkles: Succesful post and get from db

--- serverless/index.js
+++ serverless/index.js
@@ -1,10 +1,19 @@
-import serverless from 'serverless-http'
-import express from 'express'
-
+const serverless = require('serverless-http')
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+const userModel = require('./models/user')
+
+app.use(bodyParser.json({
+  strict: false
+}))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/users/:userId', userModel.getUserById)
+app.post('/users', userModel.createUser)
 
 module.exports.handler = serverless(app)
