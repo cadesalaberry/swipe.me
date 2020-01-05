50f5e5c703d4c171da3990614aa08d92c1cae09e
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-01-05 21:45:20 +0100

    Update

---
+++ docs/precache-manifest.ef07ce3a28263412a7d010bae1b33714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0edf968487b3aa9cf7c",
+    "url": "/css/app.99c4ae76.css"
+  },
+  {
+    "revision": "009c23903aef2f0155b3824bd019ae52",
+    "url": "/img/oops.009c2390.png"
+  },
+  {
+    "revision": "e49748fe786e8bf5f4b470fd7d06e7f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0edf968487b3aa9cf7c",
+    "url": "/js/app.ce25fbb2.js"
+  },
+  {
+    "revision": "66dfe7ccff77dac0e418",
+    "url": "/js/chunk-vendors.c6fcf8c9.js"
+  },
+  {
+    "revision": "5d907bca091f2e2bf05d40b0f3127611",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc34daedcb5b1d21454800f4ccb7c90f.js"
+  "/precache-manifest.ef07ce3a28263412a7d010bae1b33714.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "swipe.me"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "swipe.me"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
