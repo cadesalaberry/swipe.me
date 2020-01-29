d46edc275e2c73ad16261bc7b91af28fab63d35b
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-01-29 21:05:12 +0100

    chore(deployment): :wrench: setup deployment config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : ''
-  publicPath: '.'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : '',
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      region: 'eu-west-1',
+      bucket: 'swipeme.io',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: true,
+      pwaFiles: 'service-worker.js',
+      enableCloudfront: true,
+      cloudfrontId: 'banana',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
 }
