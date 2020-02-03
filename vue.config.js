c5150f2ccb022e16f8b019f4e5b0edf1acdf0823
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-02-03 18:10:34 +0100

    fix(server): :wrench: deploy to swipeme.io

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : ''
+  // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : ''
+  publicPath: '.'
 }
