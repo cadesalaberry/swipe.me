aac7ba5b6f1910682c5f4fa858f3ad71b1bb73f7
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-02-04 17:55:33 +0100

    feat(deployment): :rocket: deploy to s3 (#10)
    * chore(dependencies): :heavy_plus_sign: vue s3 deployer
    
    * chore(deployment): :wrench: setup deployment config
    
    * fix(deployment): :bug: deploy with the correct config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : ''
-  publicPath: '.'
+  // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : '',
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      region: 'eu-west-1',
+      bucket: 'swipeme.io',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: undefined,
+      pwa: true,
+      pwaFiles: 'service-worker.js',
+      enableCloudfront: false,
+      cloudfrontId: 'banana',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
 }
