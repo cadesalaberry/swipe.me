4745c5de9b652d964f58463ba19ae18d4bc539eb
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-03-22 19:43:15 +0100

    feat(ci): :wrench: bind frontend and backend build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const branchName = require('current-git-branch')
+const cleanBranch = (branch) => branch.replace(/[\W_]+/g, '-')
+const setEnvIfUndefined = (name, value) => { process.env[name] = process.env[name] || value }
+
+const branch = branchName()
+const stage = cleanBranch(branch)
+
+process.env.VUE_APP_VERSION = require('./package.json').version
+process.env.VUE_APP_GIT_BRANCH = branch
+process.env.VUE_APP_STAGE = stage
+
+setEnvIfUndefined('VUE_APP_COGNITO_USER_POOL_CLIENT', `${stage}-user-pool-client`)
+setEnvIfUndefined('VUE_APP_COGNITO_IDENTITY_POOL', `${stage}-identity-pool`)
+setEnvIfUndefined('VUE_APP_COGNITO_USER_POOL', `${stage}-user-pool`)
+setEnvIfUndefined('VUE_APP_S3_UPLOADS_BUCKET_NAME', `swipeme.io-${stage}`)
+setEnvIfUndefined('VUE_APP_API_BASE_URL', `https://ossn0ul273.execute-api.us-east-1.amazonaws.com/${stage}`)
+setEnvIfUndefined('VUE_APP_REGION', 'eu-west-1')
+
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? '/swipe.me/' : '',
   pluginOptions: {
