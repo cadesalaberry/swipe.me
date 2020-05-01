542162523ad34efa98d3565e8515beb1d8959097
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-05-01 17:09:34 +0200

    chore(lint): :rotating_light: fix lint issues

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-// HACK: Avoids getting -HEAD-detached-at-FETCH-HEAD- on netlify
 const branchValidator = require('./tools/branch-validator')
 
 const setEnvIfUndefined = (name, value) => { process.env[name] = process.env[name] || value }
