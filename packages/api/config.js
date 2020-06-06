a6bb6cf351e80a173a1d9b70dd40fc23dae25027
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-06-06 20:28:44 +0200

    feat(ci): :sparkles: allow deploy of long branches

--- packages/api/config.js
+++ packages/api/config.js
@@ -1,9 +1,41 @@
 const BranchValidator = require('@swipeme.io/tools/branch-validator')
 
+const hashString = (str) => {
+  let hash = 5381
+  let i = str.length
+
+  while (i) {
+    hash = (hash * 33) ^ str.charCodeAt(--i)
+  }
+
+  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+   * integers. Since we want the results to be always positive, convert the
+   * signed int to an unsigned by doing an unsigned bitshift. */
+  return `${hash >>> 0}`
+}
+
+const shortenStringToXCharacters = (string, maxSize) => {
+  if (string.length <= maxSize) { return string }
+  const hash = hashString(string)
+  const nbAvailableSlots = maxSize - hash.length
+  const breakpoint = string.length - nbAvailableSlots // start from the end of the string
+
+  return `${string.substring(breakpoint)}_${hash}`
+}
+
 module.exports = (serverless) => {
   const { stage } = serverless.variables.options
   const snakedStage = BranchValidator.snakeify(stage)
-  const config = { snakedStage }
+  const shortStageName = shortenStringToXCharacters(stage, 29)
+  const roleName = `api-swipe-me-${shortStageName}-eu-west-1-lambdaRole`
+
+  // the resulting stage name should be smaller than 64 characters
+  if (roleName.length > 64) throw new Error(`iamRoleStatementsName too long ${roleName.length}/64`)
+
+  const config = {
+    snakedStage,
+    shortStageName
+  }
 
   console.log('Injected config file:', config)
 
