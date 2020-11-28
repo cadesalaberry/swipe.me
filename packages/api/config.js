f6dbb515f3c0dfbde03b8c731a1987cf7bd948ad
C-A de Salaberry <cadesalaberry@yahoo.com>
2020-11-28 19:42:29 +0100

    fix(cognito): :bug: limit user pool domain name to 63 (#274)
    * fix(cognito): :bug: user pool domain name should not exceed 63 chars
    
    ServerlessError: An error occurred: UserPoolDomain - 1 validation error detected: Value 'swipeme-io-dependabot-npm-and-yarn-serverless-dotenv-plugin-3-1-0' at 'domain' failed to satisfy constraint: Member must have length less than or equal to 63 (Service: AWSCognitoIdentityProviderService; Status Code: 400; Error Code: InvalidParameterException; Request ID: bc3354c7-293d-4d87-89fb-fa82a9fdb255; Proxy: null).
          at /home/circleci/project/node_modules/serverless/lib/plugins/aws/lib/monitorStack.js:99:23
          at processImmediate (internal/timers.js:461:21)
      From previous event:
          at /home/circleci/project/node_modules/serverless/lib/plugins/aws/lib/monitorStack.js:27:12
    
    * chore(deps): :fire: dotenv is not needed anymore
    
    * fix(finch): :bug: finch was not using the same stage name

--- packages/api/config.js
+++ packages/api/config.js
@@ -4,20 +4,18 @@ module.exports = (serverless) => {
   const { stage } = serverless.variables.options
   const snakedStage = Brancher.snakeify(stage)
   const shortStageName = Brancher.shortenStringToXCharacters(stage, 29)
-  const roleName = `api-swipe-me-${shortStageName}-eu-west-1-lambdaRole`
+  const s3Name = Brancher.shortenStringToXCharacters(stage, 52)
   const appDomainUrl = {
-    ...{ [stage]: `https://${stage}.swipeme.io/` },
+    ...{ [stage]: `https://${s3Name}.swipeme.io/` },
     dev: 'https://www.swipeme.io/',
     undefined: 'http://localhost:8080/'
   }[stage]
 
-  // the resulting stage name should be smaller than 64 characters
-  if (roleName.length > 64) throw new Error(`iamRoleStatementsName too long ${roleName.length}/64`)
-
   const config = {
     snakedStage,
     shortStageName,
-    appDomainUrl
+    appDomainUrl,
+    s3Name
   }
 
   console.log('Injected config file:', config)
